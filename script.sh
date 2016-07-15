7a21f8c15b0a7a1e9913f7cce03364f05776dc3d
Pepe Barbe <dev@antropoide.net>
2016-07-15 14:25:44 -0500

    Debug loop

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 
 set -e -x 
 
-ssh root@$(cat FEDORA_IP.txt) -i ssh_keys/insecure dnf -y upgrade 
-ssh root@$(cat FEDORA_IP.txt) -i ssh_keys/insecure dnf -y install htop tmux  
+ssh -v root@$(cat FEDORA_IP.txt) -i ssh_keys/insecure dnf -y upgrade 
+ssh -v root@$(cat FEDORA_IP.txt) -i ssh_keys/insecure dnf -y install htop tmux  
 
